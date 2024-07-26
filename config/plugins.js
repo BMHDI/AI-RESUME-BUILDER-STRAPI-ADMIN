b7fcc84446f2c22f5dda50b1074c06f2d4a98399
BMHDI <Tarikboumehdi91@gmail.com>
2024-07-26 08:23:50 -0600

    jstsecret plugins update

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,8 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    // Other plugin configurations
+    'users-permissions': {
+      config: {
+        jwtSecret: env('JWT_SECRET', 'p+EQ1hH0QDh+O1w3wN3Q8w=='),
+      },
+    },
+  });
