4401d151499076638297e8ab745f21c73af7d682
BMHDI <Tarikboumehdi91@gmail.com>
2024-07-25 22:43:08 -0600

    Updated database confi

--- config/server.js
+++ config/server.js
@@ -2,9 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    keys: env.array('APP_KEYS', ['uFxw6Y3T+rkLIUSueuG2s10kPlJdXZE9fL+mY2uLECg=', 'jMlLNTl9gwBBXmSiTFE4gUi1+jDuoN/Uf3k3/XD4jj0=']),
   },
 });
